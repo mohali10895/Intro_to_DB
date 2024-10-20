397537a7cab27a1b69d417eb33c141d2328dd986
Mohamed Ali <88863004+mohali10895@users.noreply.github.com>
2024-10-20 23:40:03 +0400

    Update task_5.sql

--- task_5.sql
+++ task_5.sql
@@ -1,3 +1,3 @@
 -- Use the alx_book_store database
 USE alx_book_store;
-INSERT INTO Customers (customer_id, customer_name, email, address) VALUES (1, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.');
+INSERT INTO Customer (customer_id, customer_name, email, address) VALUES (1, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.');
