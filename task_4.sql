90a60492e4c7449f55613c4ae29b25324671d94e
Mohamed Ali <88863004+mohali10895@users.noreply.github.com>
2024-10-20 23:00:36 +0400

    Create task_4.sql

---
+++ task_4.sql
@@ -0,0 +1,7 @@
+-- Use the alx_book_store database
+USE alx_book_store;
+
+-- Query the INFORMATION_SCHEMA to get the full description of the books table
+SELECT COLUMN_NAME, COLUMN_TYPE, IS_NULLABLE, COLUMN_KEY, COLUMN_DEFAULT, EXTRA
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE TABLE_NAME = 'Books' AND TABLE_SCHEMA = 'alx_book_store';
